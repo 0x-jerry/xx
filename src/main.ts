b73c291711289b7bcf4da12d581fe5de51c9bf2a
0x-jerry <x.jerry.wang@gmail.com>
2021-07-14 21:10:06 +0800

    chore: remove generate completion

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import {
   Command,
   HelpCommand,
-  CompletionsCommand,
 } from 'https://deno.land/x/cliffy@v0.19.2/command/mod.ts'
 import { UpgradeCommand } from 'https://deno.land/x/cliffy@v0.19.2/command/upgrade/mod.ts'
 import { GithubProvider } from 'https://deno.land/x/cliffy@v0.19.2/command/upgrade/provider/github.ts'
@@ -14,8 +13,6 @@ await new Command()
   .default('help')
   // help
   .command('help', new HelpCommand())
-  // completions
-  .command('completions', new CompletionsCommand())
   // upgrade
   .command(
     'upgrade',
