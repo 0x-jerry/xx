461e5a8471984d41eb0e587c12fb6084e3207423
0x-jerry <x.jerry.wang@gmail.com>
2021-07-14 20:37:45 +0800

    feat: upgrade

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import {
   HelpCommand,
   CompletionsCommand,
 } from 'https://deno.land/x/cliffy@v0.19.2/command/mod.ts'
+import { UpgradeCommand } from 'https://deno.land/x/cliffy@v0.19.2/command/upgrade/mod.ts'
+import { GithubProvider } from 'https://deno.land/x/cliffy@v0.19.2/command/upgrade/provider/github.ts'
 import { config } from './conf.ts'
 
 await new Command()
@@ -14,5 +16,16 @@ await new Command()
   .command('help', new HelpCommand())
   // completions
   .command('completions', new CompletionsCommand())
+  // upgrade
+  .command(
+    'upgrade',
+    new UpgradeCommand({
+      main: 'x.ts',
+      args: ['--allow-net', '--unstable'],
+      provider: new GithubProvider({
+        repository: '0x-jerry/x',
+      }),
+    }),
+  )
   // parse
   .parse()
