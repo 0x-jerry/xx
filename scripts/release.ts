0dd65d1bcaee0b8ac0dba1f26330a693b3afd89f
0x-jerry <x.jerry.wang@gmail.com>
2021-07-14 20:54:41 +0800

    chore: add release script

---
+++ scripts/release.ts
@@ -0,0 +1,31 @@
+import { run } from '../src/utils.ts'
+import { Select } from 'https://deno.land/x/cliffy@v0.19.2/prompt/mod.ts'
+import * as semver from 'https://deno.land/x/semver@v1.4.0/mod.ts'
+import { config } from '../src/conf.ts'
+
+const inc = (type: semver.ReleaseType) => semver.inc(config.version, type)
+
+const types: semver.ReleaseType[] = ['patch', 'minor', 'major']
+
+const options = types.map((type) => ({
+  name: `${type}(${inc(type)})`,
+  value: type,
+}))
+
+const releaseType = await Select.prompt({
+  message: 'Please select release type',
+  options: options,
+})
+
+const releaseVersion = inc(releaseType as semver.ReleaseType)
+
+await run(
+  'yarn',
+  'version',
+  `--${releaseType}`,
+  '--message',
+  `chore: release ${releaseVersion}`,
+)
+
+await run('git', 'push')
+await run('git', 'push', '--tags')
