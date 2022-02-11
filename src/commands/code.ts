1d195f5d630da8c756f696f195a75262360d89f3
0x-jerry <x.jerry.wang@gmail.com>
2022-02-11 17:36:22 +0800

    fix: open recently opened file or folder

--- src/commands/code.ts
+++ src/commands/code.ts
@@ -2,6 +2,7 @@ import { Command } from 'cliffy/command/mod.ts'
 import { join } from 'path/mod.ts'
 import { run, homedir } from '../utils.ts'
 import { Confirm } from 'cliffy/prompt/confirm.ts'
+import { Select } from 'cliffy/prompt/select.ts'
 
 const codeStoragePath = join(
   homedir,
@@ -13,47 +14,68 @@ const codeStoragePath = join(
 
 export const codeCommand = new Command()
   .description('Open recent folder use vscode.')
-  .arguments('<folder:string:recent>')
-  .complete('recent', () => getVscodeRecentOpened())
-  .action(async (_, folder: string) => {
-    const matches = await getVscodeRecentOpened()
-    const hit = matches.find((f) => f.includes(folder))
+  .arguments('[folder:string:recent]')
+  .complete('recent', async () =>
+    (await getVscodeRecentOpened()).map((n) => n.label),
+  )
+  .action(async (_, folder?: string) => {
+    const recentlyOpenedItems = await getVscodeRecentOpened()
+
+    let hit =
+      folder && recentlyOpenedItems.find((f) => f.label.includes(folder))
+
     if (hit) {
       const yes = await Confirm.prompt({
-        message: `Did you mean to open: ${hit} ?`,
+        message: `Did you mean to open: ${hit.label} ?`,
         default: true,
       })
 
-      if (!yes) {
-        return
-      }
+      if (!yes) return
+    } else {
+      console.log(`Can't find file contain ${folder}, please select one.`)
+
+      const res = await Select.prompt({
+        message: 'Select recently opened file or folder:',
+        options: recentlyOpenedItems.map((n) => n.label),
+      })
+
+      hit = recentlyOpenedItems.find((f) => f.label === res)
     }
 
-    await run('code', hit || folder)
+    if (!hit) return
+
+    await run('code', hit.path)
   })
 
 async function getVscodeRecentOpened() {
   try {
     const txt = await Deno.readTextFile(codeStoragePath)
     const codeConf = JSON.parse(txt)
-    const entries = codeConf.openedPathsList.entries
 
-    const folders: string[] = []
-    const files: string[] = []
+    // ---- get recent opened history
+    const menu = codeConf.lastKnownMenubarData.menus.File.items.find(
+      (n: any) => n.submenu,
+    )
 
-    entries.forEach((e: any) => {
-      const u = new URL(e.folderUri || e.fileUri)
-      const p = decodeURIComponent(u.pathname)
+    const recentOpened: RecentlyOpened[] = menu.submenu.items
+      .filter((n: any) => n.uri)
+      .map((n: any) => {
+        return {
+          label: n.label,
+          path: n.uri.path,
+          schema: n.uri.scheme,
+        }
+      })
+    // ----
 
-      if (e.folderUri) {
-        folders.push(p)
-      } else {
-        files.push(p)
-      }
-    })
-
-    return [...folders, ...files]
+    return recentOpened
   } catch (_error) {
     return []
   }
+}
+
+interface RecentlyOpened {
+  label: string
+  path: string
+  schema: string
 }
