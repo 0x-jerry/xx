c01d020a51a5ed0de32538b65ef1113c8e08cf0b
Jerry Wang <x.jerry.wang@gmail.com>
2022-02-12 23:32:32 +0800

    feat(xr): support shell completions

---
+++ install-xr.local.sh
@@ -0,0 +1,1 @@
+deno install -r -qAf --no-check --unstable --import-map ./import_map.json ./xr.ts
--- src/main-xr.ts
+++ src/main-xr.ts
@@ -1,11 +1,32 @@
-import { version } from '../version.ts'
+import { CompletionsCommand } from 'cliffy/command/mod.ts'
+import { UpgradeCommand } from 'cliffy/command/upgrade/mod.ts'
+import { GithubProvider } from 'cliffy/command/upgrade/provider/github.ts'
 
 import { runCommand } from './commands/run.ts'
 import { debug } from './debug.ts'
+import { version } from '../version.ts'
+
+const xr = runCommand.version(version)
+
+// upgrade
+xr.command(
+  'upgrade',
+  new UpgradeCommand({
+    main: 'xr.ts',
+    importMap: 'import_map.json',
+    args: ['-A', '--no-check', '--unstable'],
+    provider: new GithubProvider({
+      repository: '0x-jerry/x',
+    }),
+  }),
+)
+
+// completion
+xr.command('completions', new CompletionsCommand())
 
 // parse
 try {
-  await runCommand.version(version).parse()
+  await xr.parse()
 } catch (e) {
   debug.error(e)
 }
