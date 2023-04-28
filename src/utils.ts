313490814c9e49bf459867b378184793d8500a35
Jerry Wang <x.jerry.wang@gmail.com>
2023-04-28 14:54:36 +0800

    chore: update deps

--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,14 @@
 export * from 'x-utils'
 
 import { run } from 'x-utils'
-import { rgb24 } from 'std/fmt/colors.ts'
 
 export async function exec(
   script: string,
   params: string[],
   env?: Record<string, string>,
 ): Promise<void> {
-  const stringified = params
-    .map((n) => (/\s/.test(n) ? JSON.stringify(n) : n))
-    .join(' ')
-
-  console.log(rgb24(['$', script, stringified].join(' '), 0x999999))
-
   await run(
     {
-      log: false,
       env,
     },
     'sh',
